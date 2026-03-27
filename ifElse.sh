631dd29f0e5e980e1c3b6014e4977929199728eb
PRIYANKA <priyanka@PRIYANKAs-MacBook-Air.local>
2026-03-27 06:34:04 +0530

    ifElse.sh result

---
+++ ifElse.sh
@@ -0,0 +1,14 @@
+# Basic if statement
+if [ $num -gt 10 ]; then
+  echo "Number is greater than 10"
+else
+  echo "Number is less or equal to 10"
+fi
+
+#for classes
+
+if [ "$class" = "DevOps" ]; then
+    echo "Welcome to devops class"
+else
+  echo "Welcome to my New class dear Students"
+fi
