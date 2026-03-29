bad735be1889e83c44b9b969e3d61aa57217f990
PRIYANKA <priyanka@PRIYANKAs-MacBook-Air.local>
2026-03-29 05:40:43 +0530

    another example of Day_Date

--- Date_Day.sh
+++ Date_Day.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-sum=$((4+5))
-echo "Sum is $sum"
+#Arithmatic example
+#Calculate sum=$((n1*n2)) and print it.
+multiply=$((17*5))
+echo "Result is $multiply"
 
-CURRENT_DAY=$(date +%d)
-echo "Today’s day is $CURRENT_DAY"
+#Date example
+#Print today’s full date with date +"%Y-%m-%d"
+TODAY=$(date +"%Y-%m-%d")
+echo "Today’s day is $TODAY"
+
+#Combine into a log message
+echo "On $TODAY the multiplication result is $multiply"
+
+# DevOps-style use case: create a backup filename
+backup_file="backup_$TODAY.tar.gz"
+echo "Backup file is $backup_file"
